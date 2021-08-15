3b0de0e783861e07207aa27e4be24f0bbd3cd1c4
Rizkifck <88756178+Rizkifck@users.noreply.github.com>
2021-08-15 23:36:07 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '6287772343860', 'anfebn', m)
-  this.sendContact(m.chat, '6287772343860', 'anfebn', m)
+  // F this.sendContact(m.chat, '6285651045302', 'Rizkifck', m)
+  this.sendContact(m.chat, '6285651045302', 'Rizkifck', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
